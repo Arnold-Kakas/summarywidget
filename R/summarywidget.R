47ba73381d63c1f6e1138b06db4a88ee29808b51
Arnold-Kakas <112856554+Arnold-Kakas@users.noreply.github.com>
2024-11-22 07:46:00 +0100

    Update summarywidget.R

--- R/summarywidget.R
+++ R/summarywidget.R
@@ -166,3 +166,36 @@ summarywidget <- function(data,
     dependencies = crosstalk::crosstalkLibs()
   )
 }
+
+#' Shiny bindings for summarywidget
+#'
+#' Output and render functions for using summarywidget within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a summarywidget
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name summarywidget-shiny
+#'
+#' @export
+summarywidgetOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'summarywidget', width, height, package = 'summarywidget')
+}
+
+#' @rdname summarywidget-shiny
+#' @export
+renderSummarywidget <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, summarywidgetOutput, env, quoted = TRUE)
+}
+
+# Use a <span> container rather than the default <div>
+summarywidget_html <- function(id, style, class, ...){
+  htmltools::tags$span(id = id, class = class)
+}
